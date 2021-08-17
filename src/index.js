415d336ede2b6537985536fba78efd127dc7c2b1
Reptylia <repty.alliance@gmail.com>
2021-08-17 22:41:20 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let lolly = [];
+    let minus = 0;
+    for(let i = 0;i < String(n).length;i++) {
+        if(String(n)[i] != '-') {
+      lolly.push(String(n)[i]);
+        } else {
+        minus = 1;
+        }
+    }
+    let poppy = lolly.reverse().join("");
+   
+    return +poppy;
 }
